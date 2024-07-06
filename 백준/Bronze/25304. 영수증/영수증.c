6f70585dd14592cd549e98872ad258328f3e3a29
ChaeyunKoo <131740574+ChaeyunKoo@users.noreply.github.com>
2024-07-07 00:59:04 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	int x;
+	int n;
+	int a, b;
+	int sum = 0;
+
+	scanf("%d", &x);
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &a, &b);
+		sum += a * b;
+	}
+	if (sum == x)
+		printf("Yes\n");
+	else
+		printf("No\n");
+
+	return 0;
+}
