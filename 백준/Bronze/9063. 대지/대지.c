a76bbe1abcd54a932e60bd5969d01843303d7a54
ChaeyunKoo <131740574+ChaeyunKoo@users.noreply.github.com>
2024-07-07 00:51:26 +0900

    [Bronze III] Title: 대지, Time: 12 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int n = 0, x = 0, y = 0;
+	scanf("%d", &n);
+
+	int maxx = -10000, minx = 10000;
+	int maxy = -10000, miny = 10000;
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &x, &y);
+		if (x >= maxx) maxx = x;
+		if (x < minx) minx = x;
+		if (y >= maxy) maxy = y;
+		if (y < miny) miny = y;
+	}
+	printf("%d", (maxx - minx) * (maxy - miny));
+	return 0;
+}
