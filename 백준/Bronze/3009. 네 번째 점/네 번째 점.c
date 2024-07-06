89c0f3095b6b7a1dae3e691a9180a484536f1a33
ChaeyunKoo <131740574+ChaeyunKoo@users.noreply.github.com>
2024-07-07 00:49:23 +0900

    [Bronze III] Title: 네 번째 점, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+	int x[3] = { 0 }, y[3] = { 0 };
+	int a = 0, b = 0;
+
+	for (int i = 0; i < 3; i++)
+		scanf("%d %d", &x[i], &y[i]);
+
+	if (x[0] == x[1])
+		a = x[2];
+	else if (x[0] == x[2])
+		a = x[1];
+	else
+		a = x[0];
+
+	if (y[0] == y[1])
+		b = y[2];
+	else if (y[0] == y[2])
+		b = y[1];
+	else
+		b = y[0];
+
+	printf("%d %d", a, b);
+	return 0;
+}
